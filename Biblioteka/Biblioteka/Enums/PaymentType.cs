09f73e6cb5547c223453f7bbe2e4e51014a62777
duledjordjevic <djordjevic.sv1.2021@uns.ac.rs>
2023-07-01 16:28:46 +0200

    Added Payment class and PaymentType enum

---
+++ Biblioteka/Biblioteka/Enums/PaymentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka.Enums
+{
+    public enum PaymentType
+    {
+        MEMBERSHIP_FEE,
+        BOOK_DAMAGE,
+        TOTAL_BOOK_DAMAGE,
+        LATE_FEE
+    }
+}
---
+++ Biblioteka/Biblioteka/Model/Payment.cs
@@ -0,0 +1,27 @@
+﻿using Biblioteka.Enums;
+using Biblioteka.Repository.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka.Model
+{
+    public class Payment : ISerializable
+    {
+        public int Id { get ; set ; }
+        public PaymentType PaymentType { get; set ; }
+        public double Amount { get; set ; }
+        public DateTime Date;
+
+        public Payment(){}
+
+        public Payment(PaymentType paymentType, double amount, DateTime date)
+        {
+            PaymentType = paymentType;
+            Amount = amount;
+            Date = date;
+        }
+    }
+}
