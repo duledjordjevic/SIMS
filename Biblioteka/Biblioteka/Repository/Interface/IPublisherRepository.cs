92fc68d56af090dd453c15d40eac27a43cee0a38
duledjordjevic <djordjevic.sv1.2021@uns.ac.rs>
2023-07-02 14:31:34 +0200

    Added PublisherRepository

---
+++ Biblioteka/Biblioteka/Repository/Interface/IPublisherRepository.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteka.Model;
+using System.Collections.Generic;
+
+namespace Biblioteka.Repository
+{
+    public interface IPublisherRepository
+    {
+        void Add(Publisher publisher);
+        Publisher Get(int id);
+        Dictionary<int, Publisher> GetAll();
+        void Remove(int id);
+        void Update(Publisher publisher);
+    }
+}
