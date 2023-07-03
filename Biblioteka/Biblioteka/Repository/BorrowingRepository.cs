3bea08ba0623f319245eb193fec0b597d441bed0
duledjordjevic <djordjevic.sv1.2021@uns.ac.rs>
2023-07-03 16:28:22 +0200

    Added BorrowingRepository

---
+++ Biblioteka/Biblioteka/Repository/BorrowingRepository.cs
@@ -0,0 +1,44 @@
+﻿using Biblioteka.Model;
+using Biblioteka.Repository.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka.Repository
+{
+    public class BorrowingRepository : IBorrowingRepository
+    {
+        private ICRUDRepository<Borrowing> _repo;
+        public BorrowingRepository(ICRUDRepository<Borrowing> repo)
+        {
+            _repo = repo;
+        }
+
+        public void Add(Borrowing borrowing)
+        {
+            _repo.Add(borrowing);
+        }
+
+        public void Update(Borrowing borrowing)
+        {
+            _repo.Update(borrowing);
+        }
+
+        public void Remove(int id)
+        {
+            _repo.Remove(id);
+        }
+
+        public Borrowing Get(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        public Dictionary<int, Borrowing> GetAll()
+        {
+            return _repo.GetAll();
+        }
+    }
+}
---
+++ Biblioteka/Biblioteka/Repository/Interface/IBorrowingRepository.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteka.Model;
+using System.Collections.Generic;
+
+namespace Biblioteka.Repository
+{
+    public interface IBorrowingRepository
+    {
+        void Add(Borrowing borrowing);
+        Borrowing Get(int id);
+        Dictionary<int, Borrowing> GetAll();
+        void Remove(int id);
+        void Update(Borrowing borrowing);
+    }
+}
