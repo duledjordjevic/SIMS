bb97aa387838d97fd3537a6007d76a5720e55e00
duledjordjevic <djordjevic.sv1.2021@uns.ac.rs>
2023-07-04 16:33:42 +0200

    Added BorrowingViewModel

---
+++ Biblioteka/Biblioteka/ViewModel/Structures/BorrowingViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Biblioteka.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka.ViewModel.Structures
+{
+    public class BorrowingViewModel : ViewModelBase
+    {
+        public Borrowing Borrowing { get; set; }
+        public BookCopy BookCopy { get; set; }
+        public BookTitle BookTitle { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public bool IsProlonged { get; set; }
+        public string Title { get; set; }
+        public BorrowingViewModel(Borrowing borrowing, BookCopy bookCopy, BookTitle bookTitle, string title) 
+        {
+            BookTitle = bookTitle;
+            Borrowing = borrowing;
+            BookCopy = bookCopy;
+            Start = Borrowing.Start;
+            End = Borrowing.End;
+            IsProlonged = Borrowing.IsProlonged;
+            Title = title;
+        }
+    }
+}
